a88eea6f9ca7c10c9b049eac9ce7f2a19b962b65
becem gharbi <becem.gharbi@live.com>
2023-09-09 11:24:55 +0100

    perf(Navbar): Lazy load dropdown

--- components/Navbar/Index.vue
+++ components/Navbar/Index.vue
@@ -1,17 +1,19 @@
 <template>
-    <naive-navbar :routes="routes" drawer-closable>
+    <naive-navbar :routes="routes" :drawer-routes="drawerRoutes" drawer-closable>
         <template #start>
             <NuxtLink to="/" class="flex items-center gap-3">
                 <NaiveIcon name="simple-icons:directus" :size="25" icon-color="#6644fe"></NaiveIcon>
-                <n-text strong>Directus</n-text>
+                <n-text strong>Directus starter</n-text>
             </NuxtLink>
         </template>
 
-        <template #end v-if="!isMobileOrTablet">
-            <n-dropdown trigger="click" :options="dropdownOptions" :style="{ padding: '8px', minWidth: '200px' }"
-                @select="handleSelect">
-                <AccountAvatar class="cursor-pointer h-8 w-8"></AccountAvatar>
-            </n-dropdown>
+        <template #end>
+            <div v-if="!isMobileOrTablet" class="notMobileOrTablet">
+                <LazyNDropdown placement="bottom-start" trigger="click" :options="dropdownOptions"
+                    :style="{ padding: '8px', minWidth: '200px' }" @select="handleDropdownSelect">
+                    <img :src="user!.avatar?.toString()" class="w-8 h-8 object-cover rounded-full ring-2 cursor-pointer" />
+                </LazyNDropdown>
+            </div>
         </template>
 
         <template #drawer-header>
@@ -19,60 +21,59 @@
         </template>
 
         <template #drawer-footer>
-            <n-button secondary block @click="() => logout()">
-                Logout
-            </n-button>
+            <n-button secondary block @click="() => logout()"> Logout </n-button>
         </template>
     </naive-navbar>
 </template>
 
 <script setup lang="ts">
-import { NaiveIcon, AccountInfo } from "#components"
-import { NavbarRoute } from "@bg-dev/nuxt-naiveui"
-import type { DropdownOption } from "naive-ui"
+import { defineAsyncComponent } from "#imports"
+import { NaiveIcon, AccountInfo } from "#components";
+import { NavbarRoute } from "@bg-dev/nuxt-naiveui";
+import type { DropdownOption } from "naive-ui";
 
-const { logout } = useDirectusAuth()
-const { isMobileOrTablet } = useNaiveDevice()
+const { logout, user } = useDirectusAuth();
+const { isMobileOrTablet } = useNaiveDevice();
 
-const routes = ref<NavbarRoute[]>([])
+const LazyNDropdown = defineAsyncComponent(() => import("naive-ui/es/dropdown/src/Dropdown"))
 
-if (isMobileOrTablet) {
-    routes.value.push({
+const routes: NavbarRoute[] = [];
+
+const drawerRoutes: NavbarRoute[] = [
+    {
         label: "Account",
+        icon: "ph:user",
         path: "/account",
-        icon: 'ph:user'
-    })
-}
+    },
+];
 
-const dropdownOptions = ref<DropdownOption[]>([
+const dropdownOptions: DropdownOption[] = [
     {
-        key: 'header',
-        type: 'render',
-        render: () => h(AccountInfo)
+        key: "header",
+        type: "render",
+        render: () => h(AccountInfo),
     },
     {
-        key: 'divider',
-        type: 'divider',
+        key: "divider",
+        type: "divider",
     },
     {
-        label: 'Account',
-        key: 'account',
-        icon: () => h(NaiveIcon, { name: 'ph:user' }),
+        label: "Account",
+        key: "account",
+        icon: () => h(NaiveIcon, { name: "ph:user" }),
     },
     {
-        label: 'Logout',
-        key: 'logout',
-        icon: () => h(NaiveIcon, { name: 'ph:sign-out' }),
-    }
-])
+        label: "Logout",
+        key: "logout",
+        icon: () => h(NaiveIcon, { name: "ph:sign-out" }),
+    },
+];
 
-
-async function handleSelect(key: string) {
-    if (key === 'logout') {
-        return logout()
-    }
-    else if (key === 'account') {
-        return navigateTo('/account')
+async function handleDropdownSelect(key: string) {
+    if (key === "logout") {
+        await logout();
+    } else if (key === "account") {
+        await navigateTo("/account");
     }
 }
 </script>
