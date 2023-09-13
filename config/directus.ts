a733560d8d0193af9b20fd30bab2b56b27dc6b9d
becem gharbi <becem.gharbi@live.com>
2023-09-13 18:27:28 +0100

    chore(config): Remove directus options fallbacks

--- config/directus.ts
+++ config/directus.ts
@@ -2,18 +2,14 @@ import type { ModuleOptions } from "@bg-dev/nuxt-directus";
 
 export const directus: Partial<ModuleOptions> = {
   rest: {
-    baseUrl: process.env.DIRECTUS_REST_BASE_URL || "http://localhost:8055",
-    nuxtBaseUrl:
-      process.env.DIRECTUS_REST_NUXT_BASE_URL || "http://localhost:3000",
+    baseUrl: process.env.DIRECTUS_REST_BASE_URL!,
+    nuxtBaseUrl: process.env.DIRECTUS_REST_NUXT_BASE_URL!,
   },
 
   graphql: {
     enabled: true,
-    httpEndpoint:
-      process.env.DIRECTUS_GRAPHQL_HTTP_ENDPOINT ||
-      "http://localhost:8055/graphql",
-    wsEndpoint:
-      process.env.DIRECTUS_GRAPHQL_WS_ENDPOINT || "ws://localhost:8055/graphql",
+    httpEndpoint: process.env.DIRECTUS_GRAPHQL_HTTP_ENDPOINT!,
+    wsEndpoint: process.env.DIRECTUS_GRAPHQL_WS_ENDPOINT,
   },
 
   auth: {
