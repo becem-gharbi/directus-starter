160823c7ffee4333cd50339809f1782f4fc5fd54
becem-gharbi <becem.gharbi@live.com>
2023-12-25 15:44:44 +0100

    refactor(default layout): Remove user check

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="page">
     <Navbar />
-    <div v-if="user" class="flex-1 container mx-auto my-8 px-4">
+    <div class="flex-1 container mx-auto my-8 px-4">
       <slot />
     </div>
     <Footer />
   </div>
 </template>
-
-<script setup>
-const { user } = useDirectusAuth()
-</script>
