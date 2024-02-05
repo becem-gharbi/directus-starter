00921ae6cab8dfb8dcccf2619dd01e4882c0b4e8
becem-gharbi <becem.gharbi@live.com>
2024-02-05 08:31:04 +0100

    chore: download icons

--- config/naiveui.ts
+++ config/naiveui.ts
@@ -1,5 +1,6 @@
 import type { ModuleOptions } from '@bg-dev/nuxt-naiveui'
 
 export const naiveui: Partial<ModuleOptions> = {
-  colorModePreference: 'light'
+  colorModePreference: 'light',
+  iconDownload: true
 }
